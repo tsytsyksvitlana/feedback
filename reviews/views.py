7c7ed419913e438b101abc86140259ab6f1ba26f
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-06-11 22:23:18 +0300

    Saved Data with a Modelform

--- reviews/views.py
+++ reviews/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
-from .models import Review
 
 # Create your views here.
 
@@ -11,10 +10,7 @@ def review(request):
     form = ReviewForm(request.POST)
 
     if form.is_valid():
-      review=Review(user_name=form.cleaned_data['user_name'],
-                    review_text=form.cleaned_data['review_text'],
-                    rating=form.cleaned_data['rating'])
-      review.save()
+      form.save()
       return HttpResponseRedirect("/thank-you")
 
   else:
