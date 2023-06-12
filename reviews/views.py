cb86f136d9e76167f16a36bb7bbb010d247746ab
tsytsyk.svitlana <tsytsyk.svetlana@gmail.com>
2023-06-12 14:13:34 +0300

    TemplateView

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -18,6 +20,10 @@ class ReviewView(View):
       return HttpResponseRedirect("/thank-you")
     return render(request, "reviews/review.html", {"form": form})
 
+class ThankYouView(TemplateView):
+  template_name="reviews/thank_you.html"
 
-def thank_you(request):
-  return render(request, "reviews/thank_you.html")
+  def get_context_data(self, **kwargs):
+    context= super().get_context_data(**kwargs)
+    context["message"]="This works!"
+    return context
